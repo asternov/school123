c93cdea70cd44b1b0d176b89726f9168f35df5df
Andrew <asternov97@yandex.ru>
2020-06-25 12:25:06 +0300

    little fix

--- public/js/app.js
+++ public/js/app.js
@@ -2389,6 +2389,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: 'youtube',
   props: ['id'],
--- resources/js/components/youtube.vue
+++ resources/js/components/youtube.vue
@@ -4,6 +4,7 @@
             <iframe style="border: 0px none; margin-left: 0px; height: 500px; margin-top: -60px; width: 100%;"
                     :src="'https://www.youtube.com/embed/' + id + '?autoplay=0&amp;enablejsapi=1&amp;rel=0;modestbranding=1&amp;showsearch=0'" scrolling="yes">
             </iframe></div>
+
     </div>
 </template>
 
