b693c99304e46fd7d2de8abf9265e42136459885
Andrew <asternov97@yandex.ru>
2020-07-07 16:24:15 +0300

    little fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -54,7 +54,7 @@ class LoginController extends Controller
             }
         }
 
-        Mail::to($request->email)->send(new NewToken($request->email));
+        Mail::to($request->post('email'))->send(new NewToken($request->post('email')));
         throw ValidationException::withMessages([
            "Ваше устройство не зарегистрированно в системе.",
             "пожалуйста, перейдите по ссылку, высланной Вам на почту."
