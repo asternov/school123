6e900b7919f7a9f3bfd1c54b337b7d2125ed2a75
Andrew <asternov97@yandex.ru>
2020-07-07 16:20:03 +0300

    little fix

---
+++ app/Mail/NewToken.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class NewToken extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $email;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(String $email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('регистрация нового устройства')->view('emails.tokens.new');
+    }
+}
