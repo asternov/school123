ef714396ef8ffdd963af66df4b6a9f217ac09a2a
andrew <asternov97@yandex.ru>
2020-10-18 14:10:00 +0300

    online school videoroom

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,10 +13,14 @@ import fa from './components/fa.vue';
 import Clipboard from 'v-clipboard'
 import VueUploadComponent from 'vue-upload-component'
 import Notifications from 'vue-notification'
+import { library, config } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 
 window.Vue = require('vue');
 const axios = require('axios').default;
 
+library.add(fas)
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -32,9 +36,20 @@ Vue.component('tinymce', tinymce);
 Vue.component('youtube', youtube);
 Vue.component('fa', fa);
 Vue.component('file-upload', VueUploadComponent)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.component('quick-edit', QuickEdit);
 Vue.use(Clipboard)
 Vue.use(Notifications)
+Vue.use(VueSimpleAlert);
+
+// Register the component globally
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
