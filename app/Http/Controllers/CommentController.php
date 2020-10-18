457636c26ec1130a0353c55699665d8a595f28dd
andrew <asternov97@yandex.ru>
2020-10-18 19:04:52 +0300

    readme update

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -12,7 +12,7 @@ class CommentController extends Controller
     public function store(CommentRequest $request, Lesson $lesson)
     {
         $data = $request->validated();
-        $data['user_id'] = auth()->id();
+        $data['user_id'] = auth()->user()->id;
         $lesson->comments()->create($data);
         return redirect('lessons/' . $lesson->id);
     }
